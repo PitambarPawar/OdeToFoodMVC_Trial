c59119e35707afd2815308917677cb3f22b0e9bd
Pitambar Pawar <pitambar.pawar243@gmail.com>
2022-03-10 12:20:33 +0530

    Initial commit with Restaurant Controller added and view which can list all restaurants. Its working correctly.

---
+++ OdeToFood.Data/Restaurant_Repo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data
+{
+    public class Restaurant_Repo
+    {
+        OdeToFoodMVCEntities db = null;
+
+        public Restaurant_Repo()
+        {
+            db = new OdeToFoodMVCEntities();
+        }
+
+        public IEnumerable<RESTAURANT> GetAll()
+        {
+            return db.RESTAURANTS.ToList();
+        }
+    }
+}
---
+++ OdeToFood.Web/Controllers/RestaurantController.cs
@@ -0,0 +1,30 @@
+﻿using OdeToFood.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Web.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        Restaurant_Repo repo = null;
+
+        public RestaurantController()
+        {
+            repo = new Restaurant_Repo();
+        }
+
+        // GET: Restaurant
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetRestaurants()
+        {
+            return View(repo.GetAll());
+        }
+    }
+}
